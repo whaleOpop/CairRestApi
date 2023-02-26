773df88657da46f0a3aed7273c040bcf1de25007
WhaleOpop <al-agabani-kit@yandex.ru>
2023-02-26 18:02:43 +0300

    Init project etc...
    add maven libs
    -Spring data jpa
    -Spring web
    -Spring mysql connector
    -Lombok
    -Spring JBDC api

---
+++ src/main/java/com/cair/api/CairRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.cair.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CairRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CairRestApiApplication.class, args);
+    }
+
+}
